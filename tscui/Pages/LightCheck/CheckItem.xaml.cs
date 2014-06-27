a8fc35494baa39f400ddf687c0dc0af0ae896edb
renhualu@live.com <renhualu@live.com>
2014-06-27 08:40:56 +0800

    增加全选功能

--- tscui/Pages/LightCheck/CheckItem.xaml.cs
+++ tscui/Pages/LightCheck/CheckItem.xaml.cs
@@ -24,5 +24,31 @@ namespace tscui.Pages.LightCheck
         {
             InitializeComponent();
         }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            R1.IsChecked = true;
+            R2.IsChecked = true;
+            R3.IsChecked = true;
+            R4.IsChecked = true;
+
+            G1.IsChecked = true;
+            G2.IsChecked = true;
+            G3.IsChecked = true;
+            G4.IsChecked = true;
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            R1.IsChecked = false;
+            R2.IsChecked = false;
+            R3.IsChecked = false;
+            R4.IsChecked = false;
+
+            G1.IsChecked = false;
+            G2.IsChecked = false;
+            G3.IsChecked = false;
+            G4.IsChecked = false;
+        }
     }
 }
